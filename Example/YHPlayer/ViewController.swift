09093d40a22f22bc55613c08c6149dba50201e0c
YEHAN <2436567084@qq.com>
2022-02-18 10:05:16 +0800

    Initial commit

---
+++ Example/YHPlayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  YHPlayer
+//
+//  Created by YEHAN on 02/17/2022.
+//  Copyright (c) 2022 YEHAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
